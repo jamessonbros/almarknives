2af454e72a4024475446b291b8034e023711d28a
Steve Jamesson <steve@groovywebdesign.com>
2014-01-06 13:46:44 -0500

    menus

--- wordpress/wp-content/themes/almar/lib/scripts.php
+++ wordpress/wp-content/themes/almar/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'edbe44e219a5522b988f50f9e7945895');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6960fae77f761a3d7ea4f0be5d873ba8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- wordpress/wp-content/themes/almar/templates/footer.php
+++ wordpress/wp-content/themes/almar/templates/footer.php
@@ -2,6 +2,19 @@
   <div class="row">
     <div class="col-lg-12">
       <?php dynamic_sidebar('sidebar-footer'); ?>
+      <div class="footer-nav-wrap">
+      <?php 
+        if (has_nav_menu('footer_navigation')):
+          wp_nav_menu(array(
+            'theme_location' => 'footer_navigation',
+            'depth' => 1,
+            'container' => false,
+            'menu_class' => 'footer-nav list-inline',
+            'fallback_cb' => false,
+          ));
+        endif;
+      ?>
+      </div>
       <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
     </div>
   </div>
