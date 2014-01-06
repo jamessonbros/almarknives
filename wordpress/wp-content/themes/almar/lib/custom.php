407221da350f78499aef41e29ef5da69e593afa5
Steve Jamesson <steve@groovywebdesign.com>
2014-01-06 15:11:58 -0500

    fix stylesheet enqueue

--- wordpress/wp-content/themes/almar/lib/custom.php
+++ wordpress/wp-content/themes/almar/lib/custom.php
@@ -9,7 +9,7 @@ add_action('init', 'almar_register_styles');
 function almar_register_styles() {
   wp_register_style('fontkit', get_stylesheet_directory_uri().'/assets/css/MyFontsWebfontsKit.css');
 }
-add_action('wp_print_scripts', 'almar_load_styles');
+add_action('wp_enqueue_scripts', 'almar_load_styles');
 function almar_load_styles() {
   wp_enqueue_style('fontkit');
 }
