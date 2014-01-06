1e2789d68f7d79459cdb2e007744b73f2ea068d6
Steve Jamesson <steve@groovywebdesign.com>
2014-01-06 12:54:10 -0500

    adding plugins

---
+++ wordpress/wp-content/plugins/bootstrap-shortcodes/js/plugins/labels.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+(function() {
+    // Creates a new plugin class and a custom listbox
+    tinymce.create('tinymce.plugins.bs_labels', {
+        createControl: function(n, cm) {
+            switch (n) {                
+                case 'bs_labels':
+                var c = cm.createSplitButton('bs_labels', {
+                    title : 'Boxes & frames shortcodes',
+                    onclick : function() {
+                    }
+                });
+
+                c.onRenderMenu.add(function(c, m) {
+                    // Boxes & frames
+                    m.add({title : 'Labels & Badges', 'class' : 'mceMenuItemTitle'}).setDisabled(1);
+                    m.add({title : 'Default', onclick : function() {
+                        tinyMCE.activeEditor.execCommand( 'mceInsertContent', false, '[bs_label]Default[/bs_label]' );
+                    }}); 
+                    m.add({title : 'Success', onclick : function() {
+                        tinyMCE.activeEditor.execCommand( 'mceInsertContent', false, '[bs_label class="success"]Default[/bs_label]' );
+                    }});
+                });
+
+                // Return the new splitbutton instance
+                return c;
+                
+            }
+            return null;
+        }
+    });
+    tinymce.PluginManager.add('bs_labels', tinymce.plugins.bs_labels);
+})();
