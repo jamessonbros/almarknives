27e37fd8725cbac1faa6fe174c8d0b42e19c409a
Steve Jamesson <steve@groovywebdesign.com>
2014-01-07 08:36:49 -0500

    switch to beige bg base theme

--- wordpress/wp-content/themes/almar/lib/scripts.php
+++ wordpress/wp-content/themes/almar/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'adae9df98e2113f4d46ffdc5a36f7e4a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0bf88767d768772b6b577d51ee9aab85');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
