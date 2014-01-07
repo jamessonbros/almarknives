420acca7e9b903ce6b971acc5608134a1ccee69b
Steve Jamesson <steve@groovywebdesign.com>
2014-01-07 12:41:03 -0500

    load favicon explicitly

--- wordpress/wp-content/themes/almar/lib/custom.php
+++ wordpress/wp-content/themes/almar/lib/custom.php
@@ -8,6 +8,18 @@
 add_theme_support('woocommerce');
 
 
+// Favicon
+add_action('wp_head', 'almar_load_favicon', 9999);
+function almar_load_favicon() {
+?>
+  
+  <link rel="icon" href="/favicon.ico">
+  <link rel="shortcut icon" href="/favicon.ico">
+
+<?php
+}
+
+
 // Custom font
 add_action('init', 'almar_register_styles');
 function almar_register_styles() {
