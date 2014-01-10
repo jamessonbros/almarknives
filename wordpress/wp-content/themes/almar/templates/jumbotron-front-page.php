38bd5d278e2992954cbc441651602b02efef8765
Steve Jamesson <steve@groovywebdesign.com>
2014-01-10 13:57:48 -0500

    carousels running

--- wordpress/wp-content/themes/almar/templates/jumbotron-front-page.php
+++ wordpress/wp-content/themes/almar/templates/jumbotron-front-page.php
@@ -1,5 +1,5 @@
 
-<div class="l-inverse">
+<div class="l-inverse carousel-container">
   <div class="container">
 
 		<div id="carousel" class="carousel slide" data-ride="carousel">
@@ -9,6 +9,7 @@
 		    <li data-target="#carousel" data-slide-to="0" class="active"></li>
 		    <li data-target="#carousel" data-slide-to="1"></li>
 		    <li data-target="#carousel" data-slide-to="2"></li>
+		    <li data-target="#carousel" data-slide-to="3"></li>
 		  </ol>
 
 		  <!-- Wrapper for slides -->
@@ -16,14 +17,26 @@
 		    <div class="item active">
 			    <h1>Eagle Classic</h1>
 			    <p>Three versions of the 4" Eagle to fit any occasion, requirement, or pocket.</p>
-			    <p><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/carousel/eagle-classic.png" class="img-responsive"></p>
+			    <p><a href="#" class="btn btn-primary btn-lg">Shop Now &raquo;</a></p>
+			    <p><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/carousel/eagle-classic.png" alt="" class="img-responsive"></p>
+		    </div>
+				<div class="item">
+			    <h1>SERE 2000</h1>
+			    <p><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/carousel/Sere2000.png" alt="" class="img-responsive"></p>
+			    <p>Better than the original, the SERE 2000 sets a new standard for tactical folding knives.</p>
 			    <p><a href="#" class="btn btn-primary btn-lg">Shop Now &raquo;</a></p>
 		    </div>
-				<div class="item active">
-			    <h1>SERE 2000</h1>
-			    <p>Better than the original, the SERE 2000 sets a new standard for tactical folding knives.</p>
-			    <p><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/carousel/Sere2000.png" class="img-responsive"></p>
-			    <p><a href="#" class="btn btn-primary btn-lg">Shop Now &raquo;</a></p>
+		    <div class="item">
+		    	<h1>UltraChef</h1>
+		    	<p><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/carousel/ultrachef.png" alt="" class="img-responsive"></p>
+		    	<p>Lorem ipsum sin dolor sit amet, consequitar in elabit sin dolor amet.</p>
+		    	<p><a href="#" class="btn btn-primary btn-lg">Shop Now &raquo;</a></p>
+		    </div>
+		    <div class="item">
+		    	<h1>UltraLights</h1>
+		    	<p><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/carousel/ultralights.png" alt="" class="img-responsive"></p>
+		    	<p>Lorem ipsum sin dolor sit amet, consequitar in elabit sin dolor amet.</p>
+		    	<p><a href="#" class="btn btn-primary btn-lg">Shop Now &raquo;</a></p>
 		    </div>
 		   </div>
 
